e543771972e3325fe27ec8aa59b07d41ba27908e
StefanoMori1106 <stefano.mori.dev@gmail.com>
2024-12-01 15:33:38 -0500

    Second Commit

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -9,7 +9,7 @@ const person = {
     role:      'Full-Stack Developer',
     avatar:    '/images/mi.png',
     location: 'Pensacola, US', // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['Japanese', 'English']  // optional: Leave the array empty if you don't want to display languages
+    languages: ['English']  // optional: Leave the array empty if you don't want to display languages
 }
 
 const newsletter = {
